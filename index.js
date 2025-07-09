bd17c0412bee2aa37e0a8d1c70014c065fe7f282
PC-GEM <godwinmangai3@gmail.com>
2025-07-09 12:58:44 +0100

    cors update

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(secureHeaders);
 app.use(hpp);
 app.use(cors({
-  origin: ['http://localhost:3000','https://ambassador-admin.vercel.app'],
+  origin: ['http://localhost:5173','https://ambassador-admin.vercel.app'],
   methods: 'GET,POST,PUT,DELETE,PATCH',
   credentials: true,
 }));
