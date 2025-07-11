4b6e5240625e2b780eaca39d1bc7d9ca19fb2a44
PC-GEM <godwinmangai3@gmail.com>
2025-07-11 12:36:35 +0100

    netlify

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(secureHeaders);
 app.use(hpp);
 app.use(cors({
-  origin: ['http://localhost:5173','https://ambassador-admin.vercel.app'],
+  origin: ['http://localhost:5173','https://ambassador-admin.vercel.app','https://ay-developers.netlify.app'],
   methods: 'GET,POST,PUT,DELETE,PATCH',
   credentials: true,
 }));
