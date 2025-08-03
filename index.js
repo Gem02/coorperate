b4552135863134033cb1452e1b1848922dce76f3
PC-GEM <godwinmangai3@gmail.com>
2025-08-03 16:02:52 +0100

    dgdghg

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(secureHeaders);
 app.use(hpp);
 app.use(cors({
-  origin: ['http://localhost:5173','https://ambassador-admin.vercel.app','https://ay-developers.netlify.app'],
+  origin: ['http://localhost:5173','https://ambassador-admin.vercel.app','https://ay-developers.netlify.app','https://admin-ambassador.netlify.app' ],
   methods: 'GET,POST,PUT,DELETE,PATCH',
   credentials: true,
 }));
